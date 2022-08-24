97e056d69378db2be49cdea96af1c133699b162a
Tassos Manganaris <tassos.manganaris@gmail.com>
2022-08-24 12:18:29 -0400

    Added ingredients with measures

--- src/components/Drink/Drink.js
+++ src/components/Drink/Drink.js
@@ -1,28 +1,24 @@
 import React, { useState, useEffect } from "react";
 import "./Drink.css";
+
 function Drink(props) {
-  const ingredients = [
-    props.strIngredient1,
-    props.strIngredient2,
-    props.strIngredient3,
-    props.strIngredient4,
-    props.strIngredient5,
-    props.strIngredient6,
-    props.strIngredient7,
-    props.strIngredient8,
-    props.strIngredient9,
-    props.strIngredient10,
-    props.strIngredient11,
-    props.strIngredient12,
-    props.strIngredient13,
-    props.strIngredient14,
-    props.strIngredient15,
-  ];
+  var ingredients = [];
+
+  for (var i = 0; i < 16; i++) {
+    const ing = props["strIngredient" + i];
+    const measure = props["strMeasure" + i];
+
+    if (ing != null && measure != null)
+      ingredients.push({i: ing, m: measure});
+  }
 
   return (
     <span>
       <div className={"drinkCard "}>
         <img src={props.strDrinkThumb} alt={props.strDrink}></img>
+        <ol className="ingList">
+          {ingredients.map((p) => <li className="ing">{p.m} of {p.i}</li>)}
+        </ol>
       </div>
     </span>
   );
